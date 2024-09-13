7b92aa234eac83158eebd8f30215de21ca7651f4
NeelPatel5150 <167676303+NeelPatel5150@users.noreply.github.com>
2024-09-13 17:23:06 +0530

    Add files via upload

---
+++ studentmanagementsystem (1).sql
@@ -0,0 +1,354 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2024 at 09:44 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `studentmanagementsystem`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ReadAllData` ()   BEGIN
+select * from student;
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(20) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`attendance_id`, `student_id`, `date`, `status`) VALUES
+(3, 114, '2024-08-19', 'Present'),
+(4, 1, '2024-08-24', 'Present'),
+(5, 114, '2024-08-25', 'Present'),
+(6, 115, '2024-08-24', 'Present'),
+(7, 116, '2024-08-24', 'Absent'),
+(8, 117, '2024-07-15', 'Absent');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `batches`
+--
+
+CREATE TABLE `batches` (
+  `batch_id` int(15) NOT NULL,
+  `batch_name` varchar(20) NOT NULL,
+  `start_date` varchar(20) NOT NULL,
+  `end_date` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `batches`
+--
+
+INSERT INTO `batches` (`batch_id`, `batch_name`, `start_date`, `end_date`) VALUES
+(11, 'B5', '2024-08-19', '2025-08-19'),
+(12, 'B4', '2024-08-24', '2025-08-24'),
+(13, 'B8', '2024-10-01', '2025-10-01'),
+(14, 'D12', '2024-01-01', '2024-06-01'),
+(15, 'A1', '2022-01-10', '2022-06-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branches`
+--
+
+CREATE TABLE `branches` (
+  `branch_id` int(20) NOT NULL,
+  `branch_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`branch_id`, `branch_name`) VALUES
+(2, 'CST'),
+(3, 'IT'),
+(4, 'CSE'),
+(5, 'AIML'),
+(6, 'RAI');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(10) NOT NULL,
+  `course_name` varchar(20) NOT NULL,
+  `credits` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course_id`, `course_name`, `credits`) VALUES
+(5, 'JAVA', 6),
+(6, 'DS', 5),
+(7, 'ES', 0),
+(8, 'DBMS', 6),
+(9, 'MATHS', 4),
+(10, 'FEE', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `enrollment_id` varchar(14) NOT NULL,
+  `student_id` int(100) NOT NULL,
+  `course_id` int(100) NOT NULL,
+  `grade` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `enrollments`
+--
+
+INSERT INTO `enrollments` (`enrollment_id`, `student_id`, `course_id`, `grade`) VALUES
+('23002171310115', 115, 6, 'A++'),
+('23002171310116', 114, 2, 'A1'),
+('23002171310119', 116, 5, 'O++'),
+('23002171310120', 117, 8, 'B+');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `exam_id` int(20) NOT NULL,
+  `course_id` int(20) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `total_marks` int(20) NOT NULL,
+  `studentid` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`exam_id`, `course_id`, `date`, `total_marks`, `studentid`) VALUES
+(2, 1, '2024-12-12', 100, 1),
+(3, 2, '2024-01-25', 52, 115),
+(4, 5, '2024-09-09', 99, 1),
+(5, 6, '2024-05-10', 25, 115),
+(6, 8, '2024-08-10', 75, 116),
+(7, 9, '2024-05-05', 65, 117);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees`
+--
+
+CREATE TABLE `fees` (
+  `fee_id` int(15) NOT NULL,
+  `student_id` int(20) NOT NULL,
+  `amount` int(250) NOT NULL,
+  `due_date` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `fees`
+--
+
+INSERT INTO `fees` (`fee_id`, `student_id`, `amount`, `due_date`) VALUES
+(1, 1, 95000, '2024-08-19'),
+(2, 114, 500000, '2024-12-01'),
+(3, 115, 95000, '2024-08-24'),
+(4, 116, 150000, '2023-12-01'),
+(5, 117, 50000, '2022-10-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `user_id` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`user_id`, `password`) VALUES
+('1234', '0'),
+('admin', '1234'),
+('Neel@2005', '2005');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `branch` varchar(20) NOT NULL,
+  `batch` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phone_number` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `name`, `branch`, `batch`, `email`, `phone_number`) VALUES
+(1, 'Namra', 'IT', 'D10', 'Namra@gmail.com', 9517531258),
+(114, 'Neel', 'CST', 'B4', 'Neel2005@gmail.com', 9998690595),
+(115, 'Nishit', 'RAI', 'B13', 'Nishit85@gmail.com', 9513578521),
+(116, 'Pratham', 'AIML', 'D2', 'Pratham52@gmail..com', 9513694568),
+(117, 'Shubham', 'CSE', 'A8', 'Shubham23@gmail.com', 8956233265);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`);
+
+--
+-- Indexes for table `batches`
+--
+ALTER TABLE `batches`
+  ADD PRIMARY KEY (`batch_id`);
+
+--
+-- Indexes for table `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`enrollment_id`),
+  ADD KEY `fk` (`student_id`);
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`exam_id`);
+
+--
+-- Indexes for table `fees`
+--
+ALTER TABLE `fees`
+  ADD PRIMARY KEY (`fee_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `batches`
+--
+ALTER TABLE `batches`
+  MODIFY `batch_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `branch_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
+  MODIFY `exam_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `fees`
+--
+ALTER TABLE `fees`
+  MODIFY `fee_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD CONSTRAINT `fk` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
